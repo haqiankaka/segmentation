daab8cfa0f02c8f805b2f73428074637136f9b45
yuqian.wang <haqiankaka@163.com>
2021-04-15 13:43:01 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 using PointCloud  = std::vector<cv::Point2f>;
 
-int neighbor_size = 3; //邻居数量
+//int neighbor_size = 3; //邻居数量
 float neibor_dist_min_threshold = 0.05;
 float neibor_range_min_threshold = 0.05;
 float density_threshold = 1; //lof算法，大于1 表示离群 ， 小于1 表示在点密集区域，等于1 表示相邻
@@ -34,6 +34,7 @@ float range_dist(float range1,float range2)
 //与它附近的点都很近，则通过检测 ，否则等待下一轮检测
 bool IsPointNear(std::vector<PointWithRange> neibor_points,PointWithRange origin)
 {
+    size_t neighbor_size = neibor_points.size();
     for(int i = 0 ; i < neighbor_size ;i ++ )
     {
         if(distance(origin.pt,neibor_points[i].pt) > neibor_dist_min_threshold)
@@ -46,6 +47,7 @@ bool IsPointNear(std::vector<PointWithRange> neibor_points,PointWithRange origin
 
 float CalcLocalReachableDensity(std::vector<PointWithRange> neibor_points,PointWithRange origin)
 {
+    size_t neighbor_size = neibor_points.size();
     std::vector<std::pair<PointWithRange,float>> neighbor_info(neighbor_size);
     std::vector<float > distance_info(neighbor_size);
 
@@ -65,28 +67,39 @@ float CalcLocalReachableDensity(std::vector<PointWithRange> neibor_points,PointW
     std::vector<float > reachable_distance(neighbor_size);
     for(size_t i = 0 ; i < neighbor_size;i++)
     {
-        reachable_distance[i] = std::max(k_distance, neighbor_info[i].second);
+        reachable_distance[i] = std::min(k_distance, neighbor_info[i].second);
     }
 
-    float sum_reachable_dist = std::accumulate(reachable_distance.begin(),reachable_distance.end(),0);
-    std::cout<<"local reachable density:"<<neighbor_size / sum_reachable_dist<<std::endl;
-    return neighbor_size / sum_reachable_dist;
+    float sum_reachable_dist = std::accumulate(reachable_distance.begin(),reachable_distance.end(),0.0f);
+    std::cout<<"local reachable density:"<<float(neighbor_size) / sum_reachable_dist<<std::endl;
+    return float(neighbor_size) / sum_reachable_dist;
 }
 
 float CalLocalOutlierFactor(std::vector<PointWithRange> neibor_points,PointWithRange origin)
 {
     //目标点的local reachable density
+    size_t neighbor_size = neibor_points.size();
     float origin_lrd = CalcLocalReachableDensity(neibor_points,origin);
 
-    //邻居点的local reachable density
+    //求邻居点的local reachable density
     std::vector<float> neighbor_lrd(neighbor_size);
     for(size_t  i= 0; i < neighbor_size; i++)
     {
-        neighbor_lrd[i] = CalcLocalReachableDensity(neibor_points, neibor_points[i]);
+        //对于每个邻居点，生成它的邻居点
+        std::vector<PointWithRange> tmp_neighbor;
+        for(size_t j = 0 ; j < neighbor_size ; j++)
+        {
+            if(j != i )
+            {
+                tmp_neighbor.push_back(neibor_points[j]);
+            }
+        }
+        tmp_neighbor.push_back(origin);
+        neighbor_lrd[i] = CalcLocalReachableDensity(tmp_neighbor, neibor_points[i]);
     }
-    float sum_neibor_lrd = std::accumulate(neighbor_lrd.begin(), neighbor_lrd.end(), 0);
-    float lof = sum_neibor_lrd/neighbor_size / origin_lrd;
-    std::cout<<"final lof value:"<<lof<<std::endl;
+    float sum_neibor_lrd = std::accumulate(neighbor_lrd.begin(), neighbor_lrd.end(), 0.0f);
+    float lof = (sum_neibor_lrd/float(neighbor_size)) / origin_lrd;
+    std::cout<<"sum neibor lrd"<<sum_neibor_lrd<<"final lof value:"<<lof<<std::endl;
     return lof;
 }
 
@@ -121,13 +134,14 @@ std::vector<bool> Segmentation(std::vector<PointWithRange> pc)
     std::vector<bool> cluster_shape(pc.size(),0);
     cluster_shape.resize(pc.size());
     std::vector<size_t> neibor_index;
+    const size_t neighbor_size = 3;
     std::vector<PointWithRange> neibor_points(neighbor_size);
     for(size_t i = 0 ; i < pc.size() ; i ++ )
     {
         //生成邻居的下标
         for(size_t j = 0; j < neighbor_size ; j ++)
         {
-            neibor_index[j] = (i + j ) % pc.size();
+            neibor_index[j] = (i + j + 1) % pc.size();
             neibor_points[j] = pc[neibor_index[j]];
         }
         if(IsThisPointBelongToNeibor(neibor_points,pc[i]))
@@ -143,6 +157,7 @@ std::vector<bool> Segmentation(std::vector<PointWithRange> pc)
 
 int main()
 {
+    size_t neighbor_size = 3;
     std::vector<PointWithRange> neibor(neighbor_size);
     neibor[0] = {0.1,cv::Point2f(1,1),};
     neibor[1] = {0.1,cv::Point2f(1,2),};
@@ -150,6 +165,6 @@ int main()
    //neibor[3] = {0.1,cv::Point2f(0.8,4),};
    // neibor[4] = {0.1,cv::Point2f(1,5),};
    //neibor[5] = {0.1,cv::Point2f(2.1,6),};
-    PointWithRange origin = {0.1,cv::Point2f (4,8)};
+    PointWithRange origin = {0.1,cv::Point2f (1.2,3.2)};
     CalLocalOutlierFactor(neibor,origin);
 }
